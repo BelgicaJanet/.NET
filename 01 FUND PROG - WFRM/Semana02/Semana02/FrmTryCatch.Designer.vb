a3b302f20bea27a87bdacc3c3f9f1c27826fd56b
BelgicaJanet <esfingebdltg@gmail.com>
2017-02-25 17:42:16 -0500

    clase2

---
+++ 01 FUND PROG - WFRM/Semana02/Semana02/FrmTryCatch.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmTryCatch
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnGenerar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.LstNumeros = New System.Windows.Forms.ListBox()
+        Me.BtnAgregar = New System.Windows.Forms.Button()
+        Me.TxtNumero = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'BtnGenerar
+        '
+        Me.BtnGenerar.Location = New System.Drawing.Point(312, 15)
+        Me.BtnGenerar.Name = "BtnGenerar"
+        Me.BtnGenerar.Size = New System.Drawing.Size(105, 25)
+        Me.BtnGenerar.TabIndex = 0
+        Me.BtnGenerar.Text = "Generar"
+        Me.BtnGenerar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Numero:"
+        '
+        'LstNumeros
+        '
+        Me.LstNumeros.FormattingEnabled = True
+        Me.LstNumeros.Location = New System.Drawing.Point(70, 61)
+        Me.LstNumeros.Name = "LstNumeros"
+        Me.LstNumeros.Size = New System.Drawing.Size(126, 199)
+        Me.LstNumeros.TabIndex = 2
+        '
+        'BtnAgregar
+        '
+        Me.BtnAgregar.Location = New System.Drawing.Point(154, 15)
+        Me.BtnAgregar.Name = "BtnAgregar"
+        Me.BtnAgregar.Size = New System.Drawing.Size(74, 32)
+        Me.BtnAgregar.TabIndex = 8
+        Me.BtnAgregar.Text = "Agregar"
+        Me.BtnAgregar.UseVisualStyleBackColor = True
+        '
+        'TxtNumero
+        '
+        Me.TxtNumero.Location = New System.Drawing.Point(70, 17)
+        Me.TxtNumero.Name = "TxtNumero"
+        Me.TxtNumero.Size = New System.Drawing.Size(78, 20)
+        Me.TxtNumero.TabIndex = 7
+        '
+        'FrmTryCatch
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(429, 269)
+        Me.Controls.Add(Me.BtnAgregar)
+        Me.Controls.Add(Me.TxtNumero)
+        Me.Controls.Add(Me.LstNumeros)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnGenerar)
+        Me.Name = "FrmTryCatch"
+        Me.Text = "FrmTryCatch"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BtnGenerar As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents LstNumeros As System.Windows.Forms.ListBox
+    Friend WithEvents BtnAgregar As System.Windows.Forms.Button
+    Friend WithEvents TxtNumero As System.Windows.Forms.TextBox
+End Class
---
+++ 01 FUND PROG - WFRM/Semana02/Semana02/FrmUsoList.vb
@@ -0,0 +1,46 @@
+﻿Public Class FrmUsoList
+
+    Private Sub BtnAgregar_Click(sender As Object, e As EventArgs) Handles BtnAgregar.Click
+        If TxtDato.Text.Trim <> "" Then
+            With LstDatos
+                .Items.Add(TxtDato.Text)
+                LblNroElem.Text = .Items.Count
+            End With
+        Else
+            MessageBox.Show("Ingrese un dato", "Mensaje")
+        End If
+        TxtDato.Text = ""
+        TxtDato.Focus()
+    End Sub
+
+    Private Sub LstDatos_Click(sender As Object, e As EventArgs) Handles LstDatos.Click
+        If LstDatos.SelectedIndex > -1 Then
+            TxtElemento.Text = LstDatos.SelectedItem()
+            TxtIndice.Text = LstDatos.SelectedIndex
+        End If
+    End Sub
+
+    Private Sub BtnEliminar_Click(sender As Object, e As EventArgs) Handles BtnEliminar.Click
+        If LstDatos.SelectedIndex > -1 Then
+            LstDatos.Items.RemoveAt(LstDatos.SelectedIndex)
+            LblNroElem.Text = LstDatos.Items.Count
+        End If
+    End Sub
+
+    Private Sub BtnEliminarTodo_Click(sender As Object, e As EventArgs) Handles BtnEliminarTodo.Click
+        LstDatos.Items.Clear()
+        LblNroElem.Text = LstDatos.Items.Count
+    End Sub
+
+    Private Sub BtnSeleccionar_Click(sender As Object, e As EventArgs) Handles BtnSeleccionar.Click
+        Dim ind As Integer
+        ind = InputBox("Ingrese Indice:", "Mensaje")
+        LstDatos.SetSelected(ind, True)
+    End Sub
+
+    Private Sub BtnLiberar_Click(sender As Object, e As EventArgs) Handles BtnLiberar.Click
+        Dim ind As Integer
+        ind = InputBox("Ingrese Indice:", "Mensaje")
+        LstDatos.SetSelected(ind, False)
+    End Sub
+End Class
