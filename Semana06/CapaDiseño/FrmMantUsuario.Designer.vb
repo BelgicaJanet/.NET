8f7e4370b2801affa6f43794709097406de08a20
BelgicaJanet <esfingebdltg@gmail.com>
2017-04-15 16:36:32 -0500

    update semana 07

--- Semana06/CapaDiseño/FrmMantUsuario.Designer.vb
+++ Semana06/CapaDiseño/FrmMantUsuario.Designer.vb
@@ -26,6 +26,20 @@ Partial Class FrmMantUsuario
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
         Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
         Me.dgUsuario = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnEditar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtApellidos = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtClave = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
         CType(Me.dgUsuario, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -37,21 +51,170 @@ Partial Class FrmMantUsuario
         Me.dgUsuario.Size = New System.Drawing.Size(340, 347)
         Me.dgUsuario.TabIndex = 0
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(449, 81)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(259, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "INGRESE UN USUARIO"
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(385, 12)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(98, 43)
+        Me.btnNuevo.TabIndex = 2
+        Me.btnNuevo.Text = "NUEVO"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'btnEditar
+        '
+        Me.btnEditar.Location = New System.Drawing.Point(519, 12)
+        Me.btnEditar.Name = "btnEditar"
+        Me.btnEditar.Size = New System.Drawing.Size(98, 43)
+        Me.btnEditar.TabIndex = 3
+        Me.btnEditar.Text = "EDITAR"
+        Me.btnEditar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(657, 12)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(98, 43)
+        Me.btnEliminar.TabIndex = 4
+        Me.btnEliminar.Text = "ELIMINAR"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(488, 123)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(18, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "ID"
+        '
+        'txtId
+        '
+        Me.txtId.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.txtId.Location = New System.Drawing.Point(519, 121)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(100, 20)
+        Me.txtId.TabIndex = 6
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(519, 156)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(197, 20)
+        Me.txtNombre.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(459, 159)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Nombre:"
+        '
+        'txtApellidos
+        '
+        Me.txtApellidos.Location = New System.Drawing.Point(519, 194)
+        Me.txtApellidos.Name = "txtApellidos"
+        Me.txtApellidos.Size = New System.Drawing.Size(197, 20)
+        Me.txtApellidos.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(459, 197)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Apellidos: "
+        '
+        'txtClave
+        '
+        Me.txtClave.Location = New System.Drawing.Point(519, 230)
+        Me.txtClave.Name = "txtClave"
+        Me.txtClave.Size = New System.Drawing.Size(197, 20)
+        Me.txtClave.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(469, 230)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Clave:"
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(472, 284)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(117, 30)
+        Me.btnGuardar.TabIndex = 13
+        Me.btnGuardar.Text = "GUARDAR"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(615, 284)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(117, 30)
+        Me.btnCancelar.TabIndex = 14
+        Me.btnCancelar.Text = "CANCELAR"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
         'FrmMantUsuario
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(796, 344)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.txtClave)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtApellidos)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnEditar)
+        Me.Controls.Add(Me.btnNuevo)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.dgUsuario)
         Me.Name = "FrmMantUsuario"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Mantenimiento - Usuario"
         CType(Me.dgUsuario, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents ToolTip1 As System.Windows.Forms.ToolTip
     Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
     Friend WithEvents dgUsuario As System.Windows.Forms.DataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnNuevo As System.Windows.Forms.Button
+    Friend WithEvents btnEditar As System.Windows.Forms.Button
+    Friend WithEvents btnEliminar As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtId As System.Windows.Forms.TextBox
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtApellidos As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtClave As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents btnGuardar As System.Windows.Forms.Button
+    Friend WithEvents btnCancelar As System.Windows.Forms.Button
 
 End Class
---
+++ Semana06/CapaDiseño/FrmReporte.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmReporte
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.BtnTorta = New System.Windows.Forms.Button()
+        Me.BtnColumna = New System.Windows.Forms.Button()
+        Me.CboCategoria = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.charText = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        CType(Me.charText, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BtnTorta
+        '
+        Me.BtnTorta.Location = New System.Drawing.Point(257, 12)
+        Me.BtnTorta.Name = "BtnTorta"
+        Me.BtnTorta.Size = New System.Drawing.Size(96, 34)
+        Me.BtnTorta.TabIndex = 2
+        Me.BtnTorta.Text = "Torta"
+        Me.BtnTorta.UseVisualStyleBackColor = True
+        '
+        'BtnColumna
+        '
+        Me.BtnColumna.Location = New System.Drawing.Point(359, 11)
+        Me.BtnColumna.Name = "BtnColumna"
+        Me.BtnColumna.Size = New System.Drawing.Size(95, 35)
+        Me.BtnColumna.TabIndex = 4
+        Me.BtnColumna.Text = "Columna"
+        Me.BtnColumna.UseVisualStyleBackColor = True
+        '
+        'CboCategoria
+        '
+        Me.CboCategoria.FormattingEnabled = True
+        Me.CboCategoria.Location = New System.Drawing.Point(22, 25)
+        Me.CboCategoria.Name = "CboCategoria"
+        Me.CboCategoria.Size = New System.Drawing.Size(162, 21)
+        Me.CboCategoria.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Categoria:"
+        '
+        'charText
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.charText.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.charText.Legends.Add(Legend1)
+        Me.charText.Location = New System.Drawing.Point(22, 52)
+        Me.charText.Name = "charText"
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.charText.Series.Add(Series1)
+        Me.charText.Size = New System.Drawing.Size(432, 300)
+        Me.charText.TabIndex = 9
+        Me.charText.Text = "Chart1"
+        '
+        'FrmReporte
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(473, 360)
+        Me.Controls.Add(Me.charText)
+        Me.Controls.Add(Me.CboCategoria)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnColumna)
+        Me.Controls.Add(Me.BtnTorta)
+        Me.Name = "FrmReporte"
+        Me.Text = "FrmReporte"
+        CType(Me.charText, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BtnTorta As System.Windows.Forms.Button
+    Friend WithEvents BtnColumna As System.Windows.Forms.Button
+    Friend WithEvents CboCategoria As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents charText As System.Windows.Forms.DataVisualization.Charting.Chart
+End Class
